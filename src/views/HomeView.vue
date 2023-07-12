ae8ea0c51fc12476bd5efd0ce2037df30049f243
Gabriela <gdemelo.mendes@gmail.com>
2023-07-12 20:43:02 -0300

    layout: finished home page layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,20 @@
 <script setup>
+import countries from "@/data/countries.json"
 import TheFilters from "@/components/TheFilters.vue";
+import CountryCard from "../components/CountryCard.vue";
 </script>
 
 <template>
   <TheFilters />
+  
+  <div
+    class="mt-8 d-flex justify-center flex-wrap" 
+    style="gap: 5rem"
+  >
+    <CountryCard
+      v-for="country in countries"
+      :key="country.alpha3Code"
+      :country="country"
+    />
+  </div>
 </template>
