8ab6aa743e860cd7bb3c181cf8d214c13835994d
Gabriela <gdemelo.mendes@gmail.com>
2023-07-12 14:48:14 -0300

    layout: added containter around page content

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
-import TheAppBar from './components/TheAppBar.vue';
+import TheAppBar from '@/components/TheAppBar.vue';
 </script>
 
 <template>
   <v-app>
     <TheAppBar />
     <v-main>
-      <router-view/>
+      <v-container class="mt-12">
+        <router-view/>
+      </v-container>
     </v-main>
   </v-app>
 </template>
