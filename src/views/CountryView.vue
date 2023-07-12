76a89209687e6aa085491bc164de29b2b09b2a79
Gabriela <gdemelo.mendes@gmail.com>
2023-07-12 20:56:52 -0300

    feature: created country page component

---
+++ src/views/CountryView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import countries from "@/data/countries.json"
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+const countryName = route.params.name
+
+const country = countries.find(c => c.name === countryName)
+</script>
+
+<template>
+  <div>{{ country.name }}</div>
+</template>
