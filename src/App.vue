34b1a052f42ce5085b3fd1a49e3edd89760d968a
Gabriela <gdemelo.mendes@gmail.com>
2023-07-12 10:46:32 -0300

    layout: added app bar to page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
+<script setup>
+import TheAppBar from './components/TheAppBar.vue';
+</script>
+
 <template>
   <v-app>
+    <TheAppBar />
     <v-main>
       <router-view/>
     </v-main>
   </v-app>
 </template>
-
-<script>
-</script>
