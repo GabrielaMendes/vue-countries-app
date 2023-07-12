197cc93db57a12792963578ce77e3ec861c2b3d5
Gabriela <gdemelo.mendes@gmail.com>
2023-07-12 10:42:19 -0300

    config: defined custom themes and icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,41 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import { aliases, md } from "vuetify/iconsets/md";
 
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  icons: {
+    defaultSet: "md",
+    aliases,
+    sets: {
+      md,
+    },
+  },
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          background: "#FDFDFD",
+          "text-primary": "#17171F",
+          "text-secondary": "#656565",
+          primary: "#009688",
+          secondary: "#80CBC4",
+        },
+      },
+      dark: {
+        dark: true,
+        colors: {
+          background: "#17171F",
+          "text-primary": "#FDFDFD",
+          "text-secondary": "#90A4AE",
+          primary: "#00BFA5",
+          secondary: "#64FFDA",
+        },
+      },
+    },
+  },
+});
